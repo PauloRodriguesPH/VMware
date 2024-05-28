c5d3de0cd91fe9ddc826a0802ce95cce53392c55
PauloRodriguesPH <paulorodriguesph@outlook.com>
2024-05-28 18:02:20 -0300

    criação do script

---
+++ Network/AddNIC.ps1
@@ -0,0 +1,34 @@
+﻿# Importar o módulo do PowerCLI
+Import-Module VMware.PowerCLI
+
+# Definir o servidor vCenter e credenciais
+$vCenterServer = "slap1820.bancobmg.com.br"
+$vCenterUser = "paulo.rodrigues@vsphere.local"
+
+# Conectar ao vCenter Server
+Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $true
+Connect-VIServer -Server $vCenterServer -User $vCenterUser
+
+# Caminho para o arquivo CSV
+$csvPath = "U:\Scripts\lista.csv"
+
+# Importar o arquivo CSV
+$vms = Import-Csv -Path $csvPath
+
+# Loop através de cada VM no CSV
+foreach ($vm in $vms) {
+    $vmName = $vm.VMName
+    $networkName = "vPG_HML_HBT_242"
+
+    # Obter a VM
+    $vmObject = Get-VM -Name $vmName
+
+    # Adicionar a placa de rede vmxnet3
+    $networkAdapter = New-NetworkAdapter -VM $vmObject -NetworkName $networkName -Type Vmxnet3
+
+    # Mensagem de sucesso
+    Write-Host "Placa de rede vmxnet3 adicionada à VM: $vmName"
+}
+
+# Desconectar do vCenter Server
+Disconnect-VIServer -Server $vCenterServer -Confirm:$false
---
+++ Network/GetNIC.ps1
@@ -0,0 +1,47 @@
+﻿# Importar o módulo do PowerCLI
+Import-Module VMware.PowerCLI
+
+# Definir o servidor vCenter e credenciais
+$vCenterServer = "slap1820.bancobmg.com.br"
+$vCenterUser = "paulo.rodrigues@vsphere.local"
+
+# Conectar ao vCenter Server
+Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $true
+Connect-VIServer -Server $vCenterServer -User $vCenterUser
+
+# Caminho para o arquivo CSV
+$csvPath = "U:\Scripts\lista.csv"
+
+# Importar o arquivo CSV
+$vms = Import-Csv -Path $csvPath
+
+# Loop através de cada VM no CSV
+foreach ($vm in $vms) {
+    $vmName = $vm.VMName
+
+    # Obter a VM
+    $vmObject = Get-VM -Name $vmName
+
+    # Obter as placas de rede da VM
+    $networkAdapters = Get-NetworkAdapter -VM $vmObject
+
+    # Exibir informações sobre a VM e suas placas de rede
+    Write-Host "VM: $vmName"
+    Write-Host "---------------------------"
+    foreach ($adapter in $networkAdapters) {
+        $adapterName = $adapter.Name
+        $networkName = $adapter.NetworkName
+        $adapterType = $adapter.Type
+        $vlanId = (Get-VirtualPortGroup -VM $vmObject -Name $networkName).VlanId
+
+        Write-Host "Adapter Name: $adapterName"
+        Write-Host "Network Name: $networkName"
+        Write-Host "Adapter Type: $adapterType"
+        Write-Host "VLAN ID: $vlanId"
+        Write-Host "---------------------------"
+    }
+    Write-Host "`n" # Adicionar uma linha em branco entre as VMs
+}
+
+# Desconectar do vCenter Server
+Disconnect-VIServer -Server $vCenterServer -Confirm:$false
